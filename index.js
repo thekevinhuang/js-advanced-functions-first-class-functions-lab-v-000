091f1924083c5bda1839e81e28b106edaa852f62
Kevin Huang <yellow.khaos@gmail.com>
2018-10-06 17:53:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function returnFirstTwoDrivers(collection) {
-
-
+  return collection.slice(0,2)
 }
 
 const returnLastTwoDrivers = function () {
